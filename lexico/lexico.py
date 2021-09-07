a9c361b2a837a4d4b435a3dcaeaf207758cd7660
Carlos Augusto <carlos.angelotti@aluno.ic.ufmt.br>
2021-09-07 02:31:10 -0400

    alteracoes no retorno do objeto token

--- lexico/lexico.py
+++ lexico/lexico.py
@@ -3,3 +3,7 @@ from auxiliares import typeConstants
 class Token:
     def __init__(self, tipo, valor = None):
         return
+
+    def __repr__(self):
+        if self.valor: return f"Tipo = {self.tipo} Valor = {self.valor}"
+        return f"Tipo = {self.tipo}"
