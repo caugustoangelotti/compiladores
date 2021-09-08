51304b10baaa01a6279084f1e6815ee100bd42c8
Carlos Augusto <carlos.angelotti@aluno.ic.ufmt.br>
2021-09-08 01:53:41 -0400

    refatoracao do lexer

--- lexico/lexer.py
+++ lexico/lexer.py
@@ -1,10 +1,1 @@
 from auxiliares import typeConstants
-
-class Token:
-    def __init__(self, _tipo, _valor = None):
-        self.tipo = _tipo
-        self.valor = _valor
-
-    def __repr__(self):
-        if self.valor: return f"[Tipo = {self.tipo} , Valor = {self.valor}]"
-        return f"[Tipo = {self.tipo}]"
