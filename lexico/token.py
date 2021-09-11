882d2eafa1c153a89192fac7134f9201e1306bca
Carlos Augusto <carlos.angelotti@aluno.ic.ufmt.br>
2021-09-11 04:15:41 -0400

    refatoracao da representacao do token

--- lexico/token.py
+++ lexico/token.py
@@ -1,8 +1,7 @@
 class Token:
-    def __init__(self, _tipo, _valor = None):
+    def __init__(self, _tipo, _valor = None) -> None:
         self.tipo = _tipo
         self.valor = _valor
-
-    def __repr__(self):
-        if self.valor: return f"[Tipo = {self.tipo} , Valor = {self.valor}]"
-        return f"[Tipo = {self.tipo}]"
+    
+    def __repr__(self) -> str:
+        return f"tkn = [{self.tipo}->{self.valor}]"
