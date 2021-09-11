41f8959871dcd7f5c378d1550bfcc981112a9be8
Carlos Augusto <carlos.angelotti@aluno.ic.ufmt.br>
2021-09-11 04:12:19 -0400

    implementacao dos testes do lexer

--- compile.py
+++ compile.py
@@ -1,3 +1,12 @@
 import lexico.lexer as lx
 
-lex = lx.Lexico("sdgfgdfg")
+with open("codigo.txt", "r", encoding='utf-8') as file:
+    fileToStringArr = file.read()
+
+lex = lx.Lexico(fileToStringArr)
+
+tknResponse = lex.nexToken()
+while tknResponse != None:
+    if(tknResponse != None):
+        print(tknResponse)
+    tknResponse = lex.nexToken()
