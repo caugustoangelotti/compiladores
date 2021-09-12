ef05316b86603f4ad7d5bce059bb6deb5d94c741
Carlos Augusto <carlos.angelotti@aluno.ic.ufmt.br>
2021-09-11 23:13:48 -0400

    tratamento de erros para garantir a integridade do arquivo fonte

--- compile.py
+++ compile.py
@@ -1,15 +1,43 @@
 import os
+import sys
 from lexico.lexer import Lexico
 
-os.system('cls')
+#os.system('cls')
+try:
+    fileDir = sys.argv[1]
+except IndexError:
+    print("Forneca o caminho do codigo fonte ex:py compile.py C:\\Users\\...")
+    exit()
 
-with open("codigo.txt", "r", encoding='utf-8') as file:
-    fileToStringArr = file.read()
+try:
+    assert (fileDir != " " and fileDir != ""), "O caminho do arquivo não pode ser vazio"
+except Exception as err:
+    print(err)
+    exit()
 
-lex = Lexico(fileToStringArr)
+try:
+    if "/" in fileDir:
+        fileExtension = fileDir.split("/")
+        fileExtension = fileExtension[len(fileExtension) - 1]
+    elif "\\" in fileDir:
+        fileExtension = fileDir.split("\\")
+        fileExtension = fileExtension[len(fileExtension) - 1]
+    else:
+        raise ValueError("O arquivo não tem um diretorio valido")
+except ValueError as err:
+    print(err)
+    exit()
 
-tknResponse = lex.nexToken()
+try:
+    fileExtension = fileExtension.split('.')
+    assert ((fileExtension[1] == 'txt') or fileExtension[1] == 'lalg'), "O arquivo deve ter extensão .txt ou .lalg"
+except Exception as err:
+    print(err)
+    exit()
+
+
+""" tknResponse = lex.nexToken()
 while tknResponse != None:
     if(tknResponse != None):
         print(tknResponse)
-    tknResponse = lex.nexToken()
+    tknResponse = lex.nexToken() """
