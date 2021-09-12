dee5bfe10610d375f68f0cf2f2b2fa11d634ea5f
Carlos Augusto <carlos.angelotti@aluno.ic.ufmt.br>
2021-09-11 23:43:02 -0400

    alteracao na geracao do arquivo de log

--- compile.py
+++ compile.py
@@ -48,10 +48,12 @@ with open(fileDir, 'r', encoding='utf-8') as file:
 
 lex = Lexico(fileToStringArr)
 
-with open("tokens.log.txt", 'w', encoding='utf-8') as arqvLog:
+with open("tokens.log.txt", 'a', encoding='utf-8') as arqvLog:
     tknResponse = lex.nexToken()
+    arqvLog.writelines("###############################\n")
     while tknResponse != None:
         if(tknResponse != None):
             print(tknResponse)
             arqvLog.writelines(f"{tknResponse.__repr__()}\n")
         tknResponse = lex.nexToken()
+    arqvLog.writelines("-------------------------------\n")
