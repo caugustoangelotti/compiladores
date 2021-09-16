11cd817ff8db990158bd88f5a5829126d34d5395
Carlos Augusto <carlos.angelotti@aluno.ic.ufmt.br>
2021-09-16 17:39:21 -0400

    complemento dos dicionarios de caracteres e palavras reservadas

--- auxiliares/reservedDic.py
+++ auxiliares/reservedDic.py
@@ -1,3 +1,40 @@
-relacoes = ['=', '<>', '>=', '<=', '>', '<']
-tipos = ['integer', 'real']
-matematicos = {'mais' : '+', 'menos' : '-', 'mult' : '*', 'div' : '/'}
+operadores = {
+    'igual' : '=',
+    'diferente' : '<>',
+    'maior_igual' : '>=',
+    'menor_igual' : '<=',
+    'maior' : '>',
+    'menor' : '<',
+    'atribui_valor' : ':='
+}
+literais = {
+    'dollar' : '$',
+    'ponto_e_virgula' : ';',
+    'virgula' : ',',
+    'abre_parenteses' : '(',
+    'fecha_parenteses' : ')',
+    'ponto' : '.',
+    'dois_pontos' : ':'
+}
+tipos = {
+    'int' : 'integer', 
+    'real' : 'real'
+}
+
+matematicos = {
+    'mais' : '+', 
+    'menos' : '-', 
+    'mult' : '*', 
+    'div' : '/'
+}
+
+words = {
+    'program' : 'program',
+    'ident' : 'ident', 
+    'write' : 'write', 
+    'read'  : 'read',
+    'if' : 'if',
+    'begin' : 'begin',
+    'end' : 'end'
+
+}
