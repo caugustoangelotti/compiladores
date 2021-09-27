296388ecba1a70964a6c648c3f2033eeba33eb40
Carlos Augusto <carlos.angelotti@aluno.ic.ufmt.br>
2021-09-27 14:30:35 -0400

    versao producao

--- compile.py
+++ compile.py
@@ -2,7 +2,7 @@ from sintatico.sintatico import Sintatico
 from lexico.lexer import Lexico
 import sys
 
-DEV = True
+DEV = False
 
 #os.system('cls')
 
@@ -12,35 +12,8 @@ else:
     try:
         fileDir = sys.argv[1]
     except IndexError:
-        print("Forneca o caminho do codigo fonte ex:py compile.py C:\\Users\\...")
+        print("Forneca o nome do arquivo ou caminho do codigo fonte ex:py compile.py C:\\Users\\...")
         exit()
-
-try:
-    assert (fileDir != " " and fileDir != ""), "O caminho do arquivo não pode ser vazio"
-except Exception as err:
-    print(err)
-    exit()
-
-#ISSUE: falso positivo em arquivos do mesmo diretorio ao não usar drag and drop
-try:
-    if "/" in fileDir:
-        fileExtension = fileDir.split("/")
-        fileExtension = fileExtension[len(fileExtension) - 1]
-    elif "\\" in fileDir:
-        fileExtension = fileDir.split("\\")
-        fileExtension = fileExtension[len(fileExtension) - 1]
-    else:
-        raise ValueError("O arquivo não tem um diretorio valido")
-except ValueError as err:
-    print(err)
-    exit()
-
-try:
-    fileExtension = fileExtension.split('.')
-    assert ((fileExtension[1] == 'txt') or fileExtension[1] == 'lalg'), "O arquivo deve ter extensão .txt ou .lalg"
-except Exception as err:
-    print(err)
-    exit()
 
 with open(fileDir, 'r', encoding='utf-8') as file:
     fileToCharArr = file.read()
