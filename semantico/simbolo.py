54b8e3d069328b8e5216cfc1861af770a1f69d04
Carlos Augusto <carlos.angelotti@aluno.ic.ufmt.br>
2021-10-12 17:02:17 -0400

    adicao campo endereco relativo

--- semantico/simbolo.py
+++ semantico/simbolo.py
@@ -1,13 +1,17 @@
 class Simbolo:
-    def __init__(self, _varName, _varType) -> None:
+    def __init__(self, _varName, _varType, _enderecoRelativo = None) -> None:
         self.varName = _varName
         self.varType = _varType
+        self.enderecoRelativo = _enderecoRelativo
 
     def __repr__(self) -> str:
-        return f"@<varName: {self.varName} varType: {self.varType}>"
+        return f"@<varName: {self.varName} varType: {self.varType} enderecoRelativo: {self.enderecoRelativo}>"
 
     def getVarName(self):
         return self.varName
 
     def getType(self):
         return self.varType
+    
+    def getEnderecoRelativo(self):
+        return self.enderecoRelativo
